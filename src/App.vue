6926b16896f2326fa7c5d502a428dc9735de3826
Ase444ka <istomina.asia@yandex.ru>
2025-11-15 14:23:56 +0300

    add structure

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,57 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <header>
+    <h1>People's blog</h1>
+    <label for="search">
+      search
+      <input type="text" id="search" />
+    </label>
+  </header>
+  <main>
+    <table>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>title</th>
+          <th>email</th>
+          <th>body</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>1</th>
+          <th>lol@lol.ru</th>
+          <th>lol@lol.ru</th>
+          <th>hello people...</th>
+        </tr>
+        <tr>
+          <th>1</th>
+          <th>lol@lol.ru</th>
+          <th>lol@lol.ru</th>
+          <th>hello people...</th>
+        </tr>
+        <tr>
+          <th>1</th>
+          <th>lol@lol.ru</th>
+          <th>lol@lol.ru</th>
+          <th>hello people...</th>
+        </tr>
+        <tr>
+          <th>1</th>
+          <th>lol@lol.ru</th>
+          <th>lol@lol.ru</th>
+          <th>hello people...</th>
+        </tr>
+        <tr>
+          <th>1</th>
+          <th>lol@lol.ru</th>
+          <th>lol@lol.ru</th>
+          <th>hello people...</th>
+        </tr>
+      </tbody>
+    </table>
+  </main>
 </template>
 
 <style scoped></style>
