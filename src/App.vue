520742ef2dc6ea22e543369fd3c9490c635767d7
Ase444ka <istomina.asia@yandex.ru>
2025-11-15 22:54:34 +0300

    add tooltip

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@ import {onMounted, useTemplateRef, ref} from 'vue'
 import {usePostStore} from '@/stores/posts'
 import {useStateStore} from '@/stores/state'
 import SortIcon from '@/icons/IconSort.vue'
+import {
+  TooltipArrow,
+  TooltipContent,
+  TooltipPortal,
+  TooltipProvider,
+  TooltipRoot,
+  TooltipTrigger,
+} from 'radix-vue'
 
 const store = usePostStore()
 const stateStore = useStateStore()
@@ -90,8 +98,25 @@ function getClass(value) {
           >
             <div class="p-1 basis-1/12">{{ post.id }}</div>
             <div class="p-1 basis-4/12">{{ post.title }}</div>
+
             <div class="p-1 basis-3/12 cursor-pointer">{{ post.email }}</div>
-            <div class="p-1 basis-4/12 cursor-pointer">{{ post.short }}...</div>
+            <TooltipProvider>
+              <TooltipRoot>
+                <TooltipTrigger class="p-1 basis-4/12 cursor-pointer">
+                  {{ post.short }}...
+                </TooltipTrigger>
+                <TooltipPortal>
+                  <TooltipContent
+                    class="w-2xl not-[]:data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade bg-amber-200 select-none rounded-sm px-[15px] py-2.5 text-[15px] leading-none shadow-md will-change-[transform,opacity]"
+                    :side-offset="2"
+                    align="end"
+                  >
+                    {{ post.body }}
+                    <TooltipArrow class="fill-amber-200" :width="8" />
+                  </TooltipContent>
+                </TooltipPortal>
+              </TooltipRoot>
+            </TooltipProvider>
           </div>
         </TransitionGroup>
       </div>
