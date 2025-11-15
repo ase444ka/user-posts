acbf74ecea95f8989e8ab54a629c74287f89d121
Ase444ka <istomina.asia@yandex.ru>
2025-11-15 22:09:09 +0300

    change base path

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/user-posts/',
   plugins: [
     vue(),
     vueDevTools(),
