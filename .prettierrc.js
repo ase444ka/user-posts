069cf64e224ad5f8e7a984830f9c4bb69dde13f0
Ase444ka <istomina.asia@yandex.ru>
2025-11-15 13:07:45 +0300

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+export default {
+  tabWidth: 2,
+  tabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  bracketSameLine: false,
+  endOfLine: 'lf',
+  arrowParens: 'avoid',
+  trailingComma: 'all',
+  bracketSpacing: false,
+}
