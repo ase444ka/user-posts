e931935304851f42495ce49596c1a5a3a39e9847
Ase444ka <istomina.asia@yandex.ru>
2025-11-17 15:10:57 +0300

    add list-header component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <script setup>
+import ListHeader from '@/components/ListHeader.vue'
 import TableSkeleton from '@/components/TableSkeleton.vue'
 import PostList from '@/components/PostList.vue'
-import {onMounted, ref} from 'vue'
+import {onMounted} from 'vue'
 import {usePostStore} from '@/stores/posts'
 import {useStateStore} from '@/stores/state'
-import SortIcon from '@/icons/IconSort.vue'
 
 const postStore = usePostStore()
 const stateStore = useStateStore()
 onMounted(postStore.getPosts)
 
-const currentSorting = ref(null)
 
-function sortPosts(field) {
-  postStore.sort(field)
-  stateStore.resetTable()
-  currentSorting.value = field
-}
-
-function getClass(value) {
-  return currentSorting.value === value ? '' : 'text-amber-300'
-}
 </script>
 
 <template>
@@ -32,39 +22,7 @@ function getClass(value) {
     </label>
   </header>
   <main class="mt-5 mx-auto w-fit">
-    <div
-      class="sticky top-0 bg-amber-200 w-2xl flex justify-between border-b border-b-gray-400"
-    >
-      <div
-        class="p-1 basis-1/12 cursor-auto select-none flex gap-3"
-        @click="sortPosts('id')"
-      >
-        id
-        <SortIcon class="flex" :class="getClass('id')" />
-      </div>
-      <div
-        class="p-1 basis-4/12 cursor-auto select-none flex gap-3"
-        @click="sortPosts('title')"
-      >
-        title
-        <SortIcon :class="getClass('title')" />
-      </div>
-
-      <div
-        class="p-1 basis-3/12 cursor-auto select-none flex gap-3"
-        @click="sortPosts('email')"
-      >
-        email
-        <SortIcon :class="getClass('email')" />
-      </div>
-      <div
-        class="p-1 basis-4/12 cursor-auto select-none flex gap-3"
-        @click="sortPosts('body')"
-      >
-        body
-        <SortIcon :class="getClass('body')" />
-      </div>
-    </div>
+    <ListHeader />
     <TableSkeleton v-if="stateStore.loading" />
     <PostList />
   </main>
---
+++ src/components/ListHeader.vue
@@ -0,0 +1,59 @@
+<template>
+  <div
+    class="sticky top-0 bg-amber-200 w-2xl flex justify-between border-b border-b-gray-400"
+  >
+    <div
+      class="p-1 basis-1/12 cursor-auto select-none flex gap-3"
+      @click="sortPosts('id')"
+    >
+      id
+      <SortIcon class="flex" :class="getClass('id')" />
+    </div>
+    <div
+      class="p-1 basis-4/12 cursor-auto select-none flex gap-3"
+      @click="sortPosts('title')"
+    >
+      title
+      <SortIcon :class="getClass('title')" />
+    </div>
+
+    <div
+      class="p-1 basis-3/12 cursor-auto select-none flex gap-3"
+      @click="sortPosts('email')"
+    >
+      email
+      <SortIcon :class="getClass('email')" />
+    </div>
+    <div
+      class="p-1 basis-4/12 cursor-auto select-none flex gap-3"
+      @click="sortPosts('body')"
+    >
+      body
+      <SortIcon :class="getClass('body')" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+import SortIcon from '@/icons/IconSort.vue'
+import {usePostStore} from '@/stores/posts'
+import {useStateStore} from '@/stores/state'
+
+const postStore = usePostStore()
+const stateStore = useStateStore()
+
+const currentSorting = ref(null)
+
+function sortPosts(field) {
+  postStore.sort(field)
+  stateStore.resetTable()
+  currentSorting.value = field
+}
+
+function getClass(value) {
+  return currentSorting.value === value ? '' : 'text-amber-300'
+}
+</script>
+
+<style scoped></style>
